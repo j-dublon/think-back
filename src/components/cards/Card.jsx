5e22929903b18ea9f03a1068ed661ef89f23879d
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2021-05-21 08:24:35 +0100

    add card component and task view

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <main className="card">
+      <h2 className="card_title">A</h2>
+    </main>
+  )
+}
+
+export default Card;
---
+++ src/components/screens/TaskView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../cards/Card";
+
+const TaskView = () => {
+  return (
+    <body>
+      <h1>Task</h1>
+      <Card />
+    </body>
+  )
+}
+
+export default TaskView;
