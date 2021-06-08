bab26a2722222dae56095af8875948913e60c0af
Jodi Dublon <jodi.dublon@thedistance.co.uk>
2021-06-08 20:42:01 +0100

    correct intro view info

--- src/components/screens/IntroView.jsx
+++ src/components/screens/IntroView.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import useData from '../../data/useData';
 import { useNavigate } from "@reach/router"
 
@@ -27,9 +27,6 @@ const IntroView = () => {
       <h4 className="intro__text">We will show you a letter...</h4>
       <div className="intro__text__container">
         <h4 className="intro__text">...if it matches the letter before last, click the tick button: <FontAwesomeIcon icon={faCheckCircle} color="#13070c" className="intro__text__icon"/></h4>
-      </div>
-      <div className="intro__text__container">
-        <h4 className="intro__text">If not, click the cross button: <FontAwesomeIcon icon={ faTimesCircle } color="#13070c" className="intro__text__icon"/></h4>
       </div>
       <h4 className="intro__text">Ready?</h4>
       <button className="intro__button" onClick={onClickStart}>
